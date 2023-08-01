ba0cc6a25982320d9e36d9b8c7e57b3c8a1a6eff
JeanneBlt <jeanne.boulet@edu.esiee.fr>
2023-08-01 21:44:44 +0200

    new menu

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import './theme.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <div>test</div>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="MenuLine">
+        <div className="grid-container">
+          <div className="Menu">Projet Personnels</div>
+          <div className="Menu">Projet Scolaires</div>
+        </div>
+      </div>
     </div>
   );
 }
