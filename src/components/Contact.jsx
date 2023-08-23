616b6bb83c23b51bf8f59758f7b1b8eb65722303
Jeanne Boulet <jeanne.boulet@aezan.com>
2023-08-23 14:32:11 +0200

    new tjwebsite

---
+++ src/components/Contact.jsx
@@ -0,0 +1,35 @@
+import SocialMedia from "./SocialMedia";
+import ContactForm from "./ContactForm";
+import SectionTitle from "./SectionTitle";
+
+const Contact = () => {
+    return (
+        <section className="py-[100px]" id="contact">
+            <div className="container">
+                <SectionTitle
+                    title="Get In Touch"
+                />
+
+                <div className="grid grid-cols-1 md:grid-cols-12">
+                    <div className="col-span-5">
+                        <div className="font-medium space-y-2">
+                            <address className="not-italic">
+                                48 allée des lentisques, Villeneuve-les-Avignon 30400
+                            </address>
+                            <p>jeanne.boulet@edu.esiee.fr</p>
+                            <p>+33619190699</p>
+                        </div>
+
+                        <SocialMedia className="mt-6 md:mt-10"/>
+                    </div>
+
+                    <div className="col-span-7 mt-10 md:mt-0">
+                        <ContactForm/>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Contact;
