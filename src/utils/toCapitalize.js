bd4c2a876ddbd09344b19afbfe78b2b787ea2056
Jeanne Boulet <jeanne.boulet@aezan.com>
2023-08-28 12:10:38 +0200

    new map condition

--- src/utils/toCapitalize.js
+++ src/utils/toCapitalize.js
@@ -5,15 +5,19 @@
  * @returns {string|*}
  */
 const toCapitalize = (string, everyWord) => {
-    string = string.trim();
-    if (everyWord) {
-        return string.split(" ").map(word => {
-            const lower = word.toLowerCase()
-            return word.charAt(0).toUpperCase() + lower.slice(1)
-        }).join(" ")
+    if (string) {
+        string = string.trim();
+        if (everyWord) {
+            return string.split(" ").map(word => {
+                const lower = word.toLowerCase();
+                return word.charAt(0).toUpperCase() + lower.slice(1);
+            }).join(" ");
+        } else {
+            const lower = string.toLowerCase();
+            return string.charAt(0).toUpperCase() + lower.slice(1);
+        }
     } else {
-        const lower = string.toLowerCase();
-        return string.charAt(0).toUpperCase() + lower.slice(1);
+        return ""; // Ou retourner un message d'erreur approprié si nécessaire
     }
 };
 
